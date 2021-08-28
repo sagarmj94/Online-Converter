29cc7fe8ed1beee890d08d412e97527c40d68dea
sagarmj94 <sagarmj1502@gmail.com>
2021-08-28 12:50:31 +0530

    2 files modified, 1 added

--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 function TextForm(props) {
-  const [text, setText] = useState('Online Converter');
+  const [text, setText] = useState('');
 
   const handleUpClick = () => {
     let newText = text.toUpperCase();
@@ -15,6 +15,11 @@ function TextForm(props) {
     console.log('habdleOnchange');
     setText(event.target.value);
   };
+  const handleClearClick = () => {
+    let newText = '';
+    setText(newText);
+  };
+
   return (
     <>
       <div className="container my-3">
@@ -31,9 +36,22 @@ function TextForm(props) {
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>
           Convert to Uppercase
         </button>
-        <button className="btn btn-primary" onClick={handleLowClick}>
+        <button className="btn btn-primary " onClick={handleLowClick}>
           Convert to LowerCase
         </button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+          Clear
+        </button>
+      </div>
+      <div className="container">
+        <h2>Your Text Summery</h2>
+        <p>
+          {text.split(' ').length} words & {text.length}
+          Charecters
+        </p>
+        <p>{0.008 * text.split(' ').length} Minutes to Read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
       </div>
     </>
   );
